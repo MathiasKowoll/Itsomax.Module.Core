d6dc517201af24138f7bf9f5dddb0860ee2686e9
Mathias Kowoll <mathias.kowoll@itsomax.com>
2018-07-26 10:42:37 -0400

    Added AppSettingModels

---
+++ ViewModels/AppSettingModels.cs
@@ -0,0 +1,10 @@
+﻿namespace Itsomax.Module.Core.ViewModels
+{
+    public class AppSettingModels
+    {
+        public string SettingType { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string KeyName { get;set; }
+    }
+}
