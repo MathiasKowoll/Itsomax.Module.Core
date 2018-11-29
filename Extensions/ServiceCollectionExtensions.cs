dcbd02d2f9e520b8a147c05f67816da162ab06a5
MathiasKowoll <mathias.kowoll@itsomax.com>
2018-11-29 15:10:26 -0300

    Implement new modules loading

--- Extensions/ServiceCollectionExtensions.cs
+++ Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -21,12 +22,15 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Newtonsoft.Json;
 
 
 namespace Itsomax.Module.Core.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        
+        /*
         public static IServiceCollection LoadInstalledModules(this IServiceCollection services, string contentRootPath)
         {
             var modules = new List<ModuleInfo>();
@@ -64,8 +68,7 @@ namespace Itsomax.Module.Core.Extensions
                         modules.Add(new ModuleInfo
                         {
                             Name = moduleFolder.Name,
-                            Assembly = assembly,
-                            Path = moduleFolder.FullName
+                            Assembly = assembly
                         });
                     }
                 }
@@ -85,7 +88,100 @@ namespace Itsomax.Module.Core.Extensions
             GlobalConfiguration.Modules = modules;
             return services;
         }
+        */
+        private static readonly IModuleConfigurationManager _modulesConfig = new ModuleConfigurationManager();
+        public static IServiceCollection AddModules(this IServiceCollection services, string contentRootPath)
+        {
+            const string moduleManifestName = "module.json";
+            var modulesFolder = Path.Combine(contentRootPath, "Modules");
+            foreach (var module in _modulesConfig.GetModules())
+            {
+                var moduleFolder = new DirectoryInfo(Path.Combine(modulesFolder, module.Id));
+                var moduleManifestPath = Path.Combine(moduleFolder.FullName, moduleManifestName);
+                if (!File.Exists(moduleManifestPath))
+                {
+                    throw new MissingModuleManifestException($"The manifest for the module '{moduleFolder.Name}' is not found.", moduleFolder.Name);
+                }
 
+                using (var reader = new StreamReader(moduleManifestPath))
+                {
+                    string content = reader.ReadToEnd();
+                    dynamic moduleMetadata = JsonConvert.DeserializeObject(content);
+                    module.Name = moduleMetadata.name;
+                    module.IsBundledWithHost = moduleMetadata.isBundledWithHost;
+                }
+
+                if(!module.IsBundledWithHost)
+                {
+                    TryLoadModuleAssembly(moduleFolder.FullName, module);
+                    if (module.Assembly == null)
+                    {
+                        throw new Exception($"Cannot find main assembly for module {module.Id}");
+                    }
+                }
+                else
+                {
+                    module.Assembly = Assembly.Load(new AssemblyName(moduleFolder.Name));
+                }
+
+                GlobalConfiguration.Modules.Add(module);
+                RegisterModuleInitializerServices(module, ref services);
+            }
+
+            return services;
+        }
+        private static void TryLoadModuleAssembly(string moduleFolderPath, ModuleInfo module)
+        {
+            const string binariesFolderName = "bin";
+            var binariesFolderPath = Path.Combine(moduleFolderPath, binariesFolderName);
+            var binariesFolder = new DirectoryInfo(binariesFolderPath);
+
+            if (Directory.Exists(binariesFolderPath))
+            {
+                foreach (var file in binariesFolder.GetFileSystemInfos("*.dll", SearchOption.AllDirectories))
+                {
+                    Assembly assembly;
+                    try
+                    {
+                        assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file.FullName);
+                    }
+                    catch (FileLoadException)
+                    {
+                        // Get loaded assembly. This assembly might be loaded
+                        assembly = Assembly.Load(new AssemblyName(Path.GetFileNameWithoutExtension(file.Name)));
+
+                        if (assembly == null)
+                        {
+                            throw;
+                        }
+
+                        string loadedAssemblyVersion = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+                        string tryToLoadAssemblyVersion = FileVersionInfo.GetVersionInfo(file.FullName).FileVersion;
+
+                        // Or log the exception somewhere and don't add the module to list so that it will not be initialized
+                        if (tryToLoadAssemblyVersion != loadedAssemblyVersion)
+                        {
+                            throw new Exception($"Cannot load {file.FullName} {tryToLoadAssemblyVersion} because {assembly.Location} {loadedAssemblyVersion} has been loaded");
+                        }
+                    }
+
+                    if (Path.GetFileNameWithoutExtension(assembly.ManifestModule.Name) == module.Id)
+                    {
+                        module.Assembly = assembly;
+                    }
+                }
+            }
+        }
+
+        private static void RegisterModuleInitializerServices(ModuleInfo module, ref IServiceCollection services)
+        {
+            var moduleInitializerType = module.Assembly.GetTypes()
+                    .FirstOrDefault(t => typeof(IModuleInitializer).IsAssignableFrom(t));
+            if ((moduleInitializerType != null) && (moduleInitializerType != typeof(IModuleInitializer)))
+            {
+                services.AddSingleton(typeof(IModuleInitializer), moduleInitializerType);
+            }
+        }
         public static IServiceCollection AddCustomizedMvc(this IServiceCollection services, IList<ModuleInfo> modules)
         {
             var mvcBuilder = services
